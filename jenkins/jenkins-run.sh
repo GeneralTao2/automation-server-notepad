b4f858cc85753bc2915ff73d0f0ce7fae091be80
tao <generaltao.md@gmail.com>
2022-09-04 21:27:51 +0300

    go

---
+++ jenkins/jenkins-run.sh
@@ -0,0 +1,16 @@
+docker run \
+  -u root \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume jenkins-docker-certs:/certs/client \
+  --env JAVA_OPTS=-Dorg.jenkinsci.plugins.ghprb.GhprbRepository.webhook.insecure=true \
+  myjenkins-blueocean:2.346.2-1
